4b40208f683e818c0b1ac71a9d3a85d21a8cc55e
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2025-01-25 22:20:23 -0300

    Animação do efeito de tela

--- projeto/recursos/jogos/enchente/cenas/EfeitoTela.gd
+++ projeto/recursos/jogos/enchente/cenas/EfeitoTela.gd
@@ -2,11 +2,43 @@ extends ColorRect
 
 export var preenchimento_minimo := 0.0
 export var preenchimento_maximo := 1.0
+export var tempo_pulso := 1
+
+onready var tween := $Tween as Tween
 
 func _ready() -> void:
 	EnchenteEstadoDeJogo.connect('dano_jogador', self, '_dano_jogador')
+	tween.repeat = true
 
-func _dano_jogador(vida: Vida) -> void:
-	preenchimento_maximo = 0.25
-	var preenchimento = range_lerp(vida.vida_atual, 0, vida.vida_maxima, preenchimento_minimo, preenchimento_maximo)
+func _dano_jogador(vida) -> void:
+	if tween.is_active():
+		tween.stop_all()
+		tween.remove_all()
 
+	if vida.vida_atual == vida.vida_maxima:
+		tween.stop_all()
+		tween.remove_all()
+		self.material.set_shader_param('dissolve_amount', 0.0)
+		return
+
+	var tempo = vida.vida_atual * tempo_pulso / 2
+	tween.interpolate_property(
+		self.material, 
+		'shader_param/dissolve_amount',
+		preenchimento_minimo,
+		preenchimento_maximo,
+		tempo,
+		Tween.TRANS_CUBIC,
+		Tween.EASE_IN
+	)
+	tween.interpolate_property(
+		self.material,
+		'shader_param/dissolve_amount',
+		preenchimento_maximo,
+		preenchimento_minimo,
+		tempo,
+		Tween.TRANS_CUBIC,
+		Tween.EASE_OUT_IN,
+		tempo / 2
+	)
+	tween.start()
